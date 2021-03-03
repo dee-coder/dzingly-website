20203e1372fcfacd1f7a09a8d0148d54f49a32e3
=bpmaurya <=realvedprakash@gmail.com>
2021-03-03 11:52:02 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from "react";
-import HomePage from './pages/HomePage';
-
+import React, { Route,Router, BrowserRouter } from "react";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
-      <HomePage/>
+    <HomePage/>
   );
 };
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,30 +1,30 @@
-import React from 'react'
-import NavBar from '../components/NavBar';
-import HomeBanner from '../components/HomeBanner';
-import HowItWorks from '../components/HowItWorks';
-import ToggelSection from '../components/ToggelSection';
-import Pricing from '../components/Pricing';
-import DesignOption from '../components/DesignOption';
-import Benifits from '../components/Benifits';
-import AboutWe from '../components/AboutWe';
-import CustomerSections from '../components/CustomerSections';
-import FooTer from '../components/FooTer';
+import React, { BrowserRouter } from "react";
+import NavBar from "../components/NavBar";
+import HomeBanner from "../components/HomeBanner";
+import HowItWorks from "../components/HowItWorks";
+import ToggelSection from "../components/ToggelSection";
+import Pricing from "../components/Pricing";
+import DesignOption from "../components/DesignOption";
+import Benifits from "../components/Benifits";
+import AboutWe from "../components/AboutWe";
+import CustomerSections from "../components/CustomerSections";
+import FooTer from "../components/FooTer";
 
 const HomePage = () => {
-    return (
-        <>
-        {/* <NavBar/> */}
-        <HomeBanner/>
-        <HowItWorks/>
-        {/* <ToggelSection/> */}
-        <Pricing/>
-        <DesignOption/>
-        <Benifits/>
-        <AboutWe/>
-        <CustomerSections/>
-        <FooTer/>
-        </>
-    )
-}
+  return (
+    <>
+      {/* <NavBar/> */}
+      <HomeBanner />
+      <HowItWorks />
+      {/* <ToggelSection/> */}
+      <Pricing />
+      <DesignOption />
+      <Benifits />
+      <AboutWe />
+      <CustomerSections />
+      <FooTer />
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
